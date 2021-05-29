481f81ddad9ff842451d84b3ac780001a44538b8
ABHISHEK OLI <oliabhi26@gmail.com>
2021-05-29 22:25:57 +0530

    Update CodingBat.java

--- CodingBat.java
+++ CodingBat.java
@@ -556,3 +556,29 @@ public List<Integer> square56(List<Integer> nums) {
     strings.replaceAll(n-> n.replace("x",""));
     return strings;
   }
+    
+    
+  /*
+Given a string and a non-negative int n, we'll say that the front of the string is the first 3 chars, or whatever is there if the string is less than length 3. Return n copies of the front;
+
+
+frontTimes("Chocolate", 2) → "ChoCho"
+frontTimes("Chocolate", 3) → "ChoChoCho"
+frontTimes("Abc", 3) → "AbcAbcAbc"*/
+    
+    public String frontTimes(String str, int n) {
+  int len = str.length();
+  String temp = "";
+  
+  if (len < 4) {
+    for (int i = 0; i < n; i++) {
+      temp += str;
+    }
+  } else {
+      for (int j = 0; j < n; j++) {
+        temp += str.substring(0,3);
+      }
+  }
+  
+  return temp;
+}
